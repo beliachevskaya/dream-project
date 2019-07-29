8bfd549106b1974e7d6213ee95e55af821dd21e3
Max <mlalx@tut.by>
2019-07-29 23:38:22 +0300

    fix: check a few errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RestorePasswordComponent } from './auth/restore-password/restore-password.component';
 import { SetNewPasswordComponent } from './auth/set-new-password/set-new-password.component';
 import { LinkExpiredComponent } from './auth/link-expired/link-expired.component';
-import { CompanySettingsComponent } from "./company-settings/company-settings.component";
-import { PageComponent } from "./core/page/page.component";
-import { NextPageComponent} from './auth/next-page/next-page.component';
+import { CompanySettingsComponent } from './company-settings/company-settings.component';
+import { PageComponent } from './core/page/page.component';
+import { NextPageComponent } from './auth/next-page/next-page.component';
+
+const itemMenu: Routes = [
+  { path: 'company-settings', component: CompanySettingsComponent }
+];
 
 const routes: Routes = [
-  { path: '', component: PageComponent },
-  { path: 'signup', component: SignupComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'restore-password', component: RestorePasswordComponent},
-  { path: 'set-new-password', component: SetNewPasswordComponent},
-  { path: 'link-expired', component: LinkExpiredComponent},
-  { path: 'next-page', component: NextPageComponent}
+  { path: '', component: PageComponent, children: itemMenu },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'restore-password', component: RestorePasswordComponent },
+  { path: 'set-new-password', component: SetNewPasswordComponent },
+  { path: 'link-expired', component: LinkExpiredComponent },
+  { path: 'next-page', component: NextPageComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
