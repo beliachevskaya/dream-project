80b3bf02fdaed10d06e6d351bcaf7df4cbc9cf3a
beliachevskaya <beliachevskaya@tut.by>
2019-07-31 00:23:37 +0300

    bind: auth pages and start page

--- src/app/auth/set-new-password/set-new-password.component.ts
+++ src/app/auth/set-new-password/set-new-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-set-new-password',
@@ -7,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetNewPasswordComponent implements OnInit {
   hide = false;
-  
-  constructor() { }
+  newPassword: string;
+
+  constructor(private router: Router, private authService: AuthService, private angularFireAuth: AngularFireAuth) {
+  }
 
   ngOnInit() {
   }
-
+  onSubmit(form: NgForm) {
+    return this.angularFireAuth.auth.currentUser.updatePassword(form.value.password)
+    .then(() => { alert('Password changed');
+                  this.router.navigate(['/']);
+    })
+    .catch(error => {
+      // An error happened.
+    });
+  }
 }
