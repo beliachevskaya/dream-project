26d7dbe23d16c6bb208aa3cc1a5793260a360923
BigGlebovsky <3696732@mail.ru>
2019-08-08 23:06:28 +0300

    ref: change component name

--- src/app/timelogs/timelogs.module.ts
+++ src/app/timelogs/timelogs.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 
-import { HeadComponent } from './head/head.component';
+import { HeaderTimelogsComponent } from './header-timelogs/header-timelogs.component';
 import { TimesheetByWeekComponent } from './timesheet-by-week/timesheet-by-week.component';
 import { TimelogComponent } from './timelog/timelog.component';
 import { MaterialModule } from '../material.module';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TimelogSelectComponent } from './timelog-select/timelog-select.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     TimesheetByWeekComponent,
     TimelogComponent,
-    HeadComponent,
+    HeaderTimelogsComponent,
     TimelogSelectComponent,
   ],
   imports: [
     RouterModule,
     MaterialModule,
     CommonModule,
+    FormsModule
   ],
   entryComponents: [
   ],
