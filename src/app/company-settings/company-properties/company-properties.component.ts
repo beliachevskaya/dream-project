ef7f0a54b7a53c4b0f2fb7c6c1e57e43d36e2259
Max <mlalx@tut.by>
2019-08-10 11:28:24 +0300

    feat: remove data from child to parent

--- src/app/company-settings/company-properties/company-properties.component.ts
+++ src/app/company-settings/company-properties/company-properties.component.ts
@@ -1,12 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
-interface IcompanyProperties {
-  name: string;
-  activities: any[];
-  defaultProjects: any[];
-  startWeekDay: string;
-  workload: any[];
-}
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { IcompanyProperties, WeekDay } from '../company-settings.component';
 
 @Component({
   selector: 'app-company-properties',
@@ -14,35 +7,20 @@ interface IcompanyProperties {
   styleUrls: ['./company-properties.component.sass']
 })
 export class CompanyPropertiesComponent implements OnInit {
-  private cp: IcompanyProperties = {
-    name: 'Microsoft inc.',
-    activities: [
-      { label: 'Vacation', checked: true },
-      { label: 'Sick Leave', checked: false },
-      { label: 'Business Trip', checked: true }
-    ],
-    defaultProjects: [
-      { project: 'Adaptation', checked: true },
-      { project: 'Adaptation2', checked: true },
-      { project: 'Adaptation3', checked: false }
-    ],
-    startWeekDay: 'Sunday',
-    workload: [35, 'week']
-  };
+  _company: IcompanyProperties;
+  public startWeekDay = [['Sunday', 'Monday'], 'Monday'];
 
-  public name: string;
-  public activities: any[];
-  public defaultProjects: any[] = [];
-  public startWeekDay: any[] = [['Sunday', 'Monday'], 'Monday'];
-  public workload: any[] = [40, 'week'];
-
-  constructor() {
-    this.name = this.cp.name;
-    this.activities = this.cp.activities;
-    this.defaultProjects = this.cp.defaultProjects;
-    this.startWeekDay[1] = this.cp.startWeekDay;
-    this.workload = this.cp.workload;
+  @Input()
+  set company(Company: IcompanyProperties) {
+    this._company = Company;
+    this.startWeekDay[1] = this._company.startWeekDay;
   }
 
-  ngOnInit() {}
+  constructor() {
+    console.log(this._company);
+  }
+
+  ngOnInit() {
+    console.log(this._company);
+  }
 }
--- src/app/company-settings/company-settings.component.ts
+++ src/app/company-settings/company-settings.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+export const enum WeekDay {
+  Sun = 'Sunday',
+  Mon = 'Monday'
+}
+
+export interface IcompanyProperties {
+  name: string;
+  activities: any[];
+  defaultProjects: any[];
+  startWeekDay: WeekDay;
+  workload: [number, string];
+}
 
 @Component({
   selector: 'app-company-settings',
@@ -6,5 +18,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-settings.component.sass']
 })
 export class CompanySettingsComponent implements OnInit {
+  public Company: IcompanyProperties = {
+    name: 'Microsoft inc.',
+    activities: [
+      { label: 'Vacation', checked: true },
+      { label: 'Sick Leave', checked: false },
+      { label: 'Business Trip', checked: true }
+    ],
+    defaultProjects: [
+      { project: 'Adaptation', checked: true },
+      { project: 'Adaptation2', checked: true },
+      { project: 'Adaptation3', checked: false }
+    ],
+    startWeekDay: WeekDay.Mon,
+    workload: [35, 'week']
+  };
   ngOnInit() {}
 }
