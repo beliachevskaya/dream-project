5797f6becf55843a14391c0ad2d963c381d05fe5
BigGlebovsky <3696732@mail.ru>
2019-08-15 23:25:37 +0300

    ref: add some changes for setData.

--- src/app/timelogs/data.service.ts
+++ src/app/timelogs/data.service.ts
@@ -3,18 +3,18 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Data} from './timelog-day/timelog-day.component';
 
-const data2: Data = {
-  timesheets: [
-    {project: 'Windows', time: 2.25, comment: '#435: added localization on landing page'},
-    {project: 'Microsoft', time: 4.5, comment: 'never work'}
-  ],
-  projects: [
-    {name: 'Windows', color: 'red'},
-    {name: 'Skype', color: 'yellow'},
-    {name: 'Mifot', color: 'blue'},
-    {name: 'Microsoft', color: 'green'}
-  ]
-};
+// const data2: Data = {
+//   timesheets: [
+//     {project: 'Windows', time: 2.25, comment: '#435: added localization on landing page'},
+//     {project: 'Microsoft', time: 4.5, comment: 'never work'}
+//   ],
+//   projects: [
+//     {name: 'Windows', color: 'red'},
+//     {name: 'Skype', color: 'yellow'},
+//     {name: 'Mifot', color: 'blue'},
+//     {name: 'Microsoft', color: 'green'}
+//   ]
+// };
 
 @Injectable()
 
@@ -35,7 +35,7 @@ export class DataService {
       .append('f', 'UPDATE')
       .append('n', 'GLEB_TIMESHEET')
       .append('p', '12345')
-      .append('v', JSON.stringify(data2));
+      .append('v', JSON.stringify(value));
 
     this.http
       .post('https://fe.it-academy.by/AjaxStringStorage2.php', lock)
