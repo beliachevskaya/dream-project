155902362749e397570df9af09dcc6032def89d5
BigGlebovsky <3696732@mail.ru>
2019-08-15 23:23:47 +0300

    ref: add setData for save date on server.

--- src/app/timelogs/data.service.ts
+++ src/app/timelogs/data.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {Data} from './timelog-day/timelog-day.component';
+
+const data2: Data = {
+  timesheets: [
+    {project: 'Windows', time: 2.25, comment: '#435: added localization on landing page'},
+    {project: 'Microsoft', time: 4.5, comment: 'never work'}
+  ],
+  projects: [
+    {name: 'Windows', color: 'red'},
+    {name: 'Skype', color: 'yellow'},
+    {name: 'Mifot', color: 'blue'},
+    {name: 'Microsoft', color: 'green'}
+  ]
+};
 
 @Injectable()
 
@@ -11,12 +25,8 @@ export class DataService {
   ) {
   }
 
-  getData() {
-    const read = new HttpParams()
-      .append('f', 'READ')
-      .append('n', 'GLEB_TIMESHEET');
-
-    const block = new HttpParams()
+  setData(value) {
+    const lock = new HttpParams()
       .append('f', 'LOCKGET')
       .append('n', 'GLEB_TIMESHEET')
       .append('p', '12345');
@@ -25,7 +35,23 @@ export class DataService {
       .append('f', 'UPDATE')
       .append('n', 'GLEB_TIMESHEET')
       .append('p', '12345')
-      .append('v', 'hello');
+      .append('v', JSON.stringify(data2));
+
+    this.http
+      .post('https://fe.it-academy.by/AjaxStringStorage2.php', lock)
+      .subscribe((requestValue) => {
+        this.http
+          .post('https://fe.it-academy.by/AjaxStringStorage2.php', update)
+          .subscribe((requestValue2) => {
+            console.log(requestValue2);
+          });
+      });
+  }
+
+  getData() {
+    const read = new HttpParams()
+      .append('f', 'READ')
+      .append('n', 'GLEB_TIMESHEET');
 
     return this.http
       .post('https://fe.it-academy.by/AjaxStringStorage2.php', read);
