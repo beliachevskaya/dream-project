2d71851d9f94d4006e56e4333ab81db6416ffe1a
BigGlebovsky <3696732@mail.ru>
2019-08-25 23:15:09 +0300

    ref: add OnPush in decotrator and fix some mistakes.

--- src/app/timelogs/timelog-select/timelog-select.component.ts
+++ src/app/timelogs/timelog-select/timelog-select.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {Timesheet} from '../timelog-day/timelog-day.component';
 import {Project} from '../timelog-day/timelog-day.component';
 
 @Component({
   selector: 'app-timelog-select',
   templateUrl: './timelog-select.component.html',
-  styleUrls: ['./timelog-select.component.sass']
+  styleUrls: ['./timelog-select.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TimelogSelectComponent implements OnInit {
 
@@ -21,12 +22,13 @@ export class TimelogSelectComponent implements OnInit {
   project: object = null;
 
   ngOnInit() {
+    const firstElement = 0;
     if (this.timesheets.project !== '') {
     this.project = this.projects.filter((elem) => {
       if (elem.name === this.timesheets.project) {
         return elem;
       }
-    })[0];
+    })[firstElement];
     }
   }
 
