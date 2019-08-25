0ded466fe4720cacd128fc45e244f92e7e1981f3
BigGlebovsky <3696732@mail.ru>
2019-08-25 23:13:45 +0300

    ref: add autosave for timesheetForm, add save data on server, delete extra code make better code.

--- src/app/timelogs/timelog-day/timelog-day.component.ts
+++ src/app/timelogs/timelog-day/timelog-day.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
-import {FormControl, FormGroup} from '@angular/forms';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, OnDestroy, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {DataService} from '../data.service';
+import {HeaderTimelogsComponent} from '../header-timelogs/header-timelogs.component';
+import * as moment from 'moment';
+import {Moment} from 'moment';
+import {auditTime, debounceTime} from 'rxjs/operators';
 
 export interface Timesheet {
   project: string;
@@ -14,94 +17,133 @@ export interface Project {
   color: string;
 }
 
+export interface Timesheets {
+  date: string;
+  saveTime: string;
+  timesheet: Timesheet[];
+}
+
 export interface Data {
-  timesheets: Timesheet[];
+  timesheets: Timesheets[];
   projects: Project[];
 }
+
+const currentDate = moment().format('L');
+
+const  saveTimeout = 2000;
+
+const minLength = 1;
 
 @Component({
   selector: 'app-timelog-day',
   templateUrl: './timelog-day.component.html',
-  styleUrls: ['./timelog-day.component.sass']
+  styleUrls: ['./timelog-day.component.sass'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 
 })
-export class TimelogDayComponent implements OnInit{
+export class TimelogDayComponent implements OnInit, OnDestroy {
+
+  @ViewChild(HeaderTimelogsComponent, {static: false})
+  private header: HeaderTimelogsComponent;
 
   timesheetForm: FormGroup;
+  data: Data = null;
+  project: Project = null;
+  displayedColumns: string[] = ['project', 'time', 'comment'];
+  date: string = moment().format('L');
+  time: string = null;
+  projects: Project[] = [];
+  timesheets: Timesheet[] = [];
+  valueChangesSubscription;
+
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private cdr: ChangeDetectorRef
   ) {
     this.timesheetForm = new FormGroup({
       project: new FormControl(null),
-      time: new FormControl('0. 0'),
+      time: new FormControl(''),
       comment: new FormControl('')
     });
-
-
+    this.valueChangesSubscription  = this.timesheetForm.valueChanges.pipe(debounceTime(saveTimeout))
+      .subscribe(formData => {
+        // console.log(formData);
+        this.saveData();
+      });
   }
 
-  focus = false;
-  project: Project = null;
-
-  data: Data = {
-    timesheets: [
-      {project: null, time: null, comment: null}
-    ],
-    projects: [
-      {name: null, color: null }
-    ]
-  };;
-
-  dataSource;
-  displayedColumns = ['project', 'time', 'comment'];
-
-  log() {
-    console.log('change');
-    // if (this.focus === false) {
-    //   this.focus = true;
-    //   setTimeout(() => {
-    //     this.saveTimesheet(this.timesheetForm.value);
-    //     this.resetTimesheet(this.timesheetForm);
-    //     this.focus = false;
-    //   }, 1000);
-    // }
+  test() {
+    console.log(this.header.datepicker.date.value.format('L'));
   }
 
-  resetTimesheet(form) {
-    form.reset();
+  resetTimesheet(): void {
+    this.timesheetForm.reset({
+      project: null,
+      time: '',
+      comment: ''
+    });
   }
-
-  // saveTimesheet(date) {
-  //   for (const key in date) {
-  //     if (key === 'project') {
-  //       const name = 'project';
-  //       console.log(date[name]);
-  //     }
-  //     if (key === 'time') {
-  //       const name = 'time';
-  //       console.log(date[name]);
-  //     }
-  //     if (key === 'comment') {
-  //       const name = 'comment';
-  //       console.log(date[name]);
-  //     }
-  //   }
-  // }
 
   deleteTimesheet(index: number): void {
-    this.dataSource.data.splice(index, 1);
-    this.dataSource = new MatTableDataSource<Timesheet>( this.dataSource.data );
+    this.timesheets = this.timesheets.filter((elem, i ) => i !== index);
+    this.date = this.header.datepicker.date.value.format('L');
+    this.time = moment().format('LT');
+    this.dataService.setTimesheetData(this.timesheets, this.date, this.time);
   }
 
   getTotalCost(): number {
-    return this.data.timesheets.map(t => t.time).reduce((acc, value) => acc + value, 0);
+    return this.timesheets
+      .map(timesheet => timesheet.time)
+      .reduce((acc, hours) => acc + hours, 0);
   }
 
-  ngOnInit() {
+  saveData(): void {
+    const {comment} = this.timesheetForm.value;
+    let {time, project} = this.timesheetForm.value;
+    project = project === null ? null : project.name;
+    time = isNaN(parseFloat(time)) ? 0 : time;
+    const validTime = !isNaN(parseFloat(time)) && parseFloat(time) !== 0;
+    const validComment = comment.length >= minLength;
+    if (validTime || validComment) {
+      const newTimesheet = {
+        project,
+        time: parseFloat(time),
+        comment
+      };
+      this.timesheets.push(newTimesheet);
+      this.timesheets = [...this.timesheets];
+      this.date = this.header.datepicker.date.value.format('L');
+      this.time = moment().format('LT');
+      this.dataService.setTimesheetData(this.timesheets, this.date, this.time);
+      this.resetTimesheet();
+    }
+  }
+
+  setData(data, timesheets: Timesheets): void {
+    const {date, saveTime, timesheet} = timesheets ? timesheets : {date: currentDate, saveTime: null, timesheet: []};
+    this.date = date;
+    this.time = saveTime;
+    this.timesheets = timesheet;
+    this.data = data || null;
+    this.projects = data.projects || [];
+  }
+
+  ngOnInit(): void {
     this.dataService.getData()
-      .subscribe((data: {result: string}) => {
-        this.data = JSON.parse(data.result);
-        this.dataSource = new MatTableDataSource<Timesheet>( this.data.timesheets );
+      .subscribe((data: Data) => {
+        if (data) {
+        const firstElement = 0;
+        const currentTimesheet = data.timesheets.filter((timesheet) => {
+          if (currentDate === timesheet.date) {
+            return timesheet;
+          }
+        });
+        this.setData(data, currentTimesheet[firstElement]);
+        }
       });
+  }
+
+  ngOnDestroy(): void {
+    this.valueChangesSubscription.unsubscribe();
   }
 }
